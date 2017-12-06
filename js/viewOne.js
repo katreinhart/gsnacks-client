c08bb62cea1cfcc6cdcec3f3db8d4f86567edabb
Kat Reinhart <kat.reinhart@gmail.com>
2017-12-05 16:03:54 -0800

    Remove rogue console.log

--- js/viewOne.js
+++ js/viewOne.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 
 function getSnack(id) {
   return axios.get(`${baseURL}/api/snacks/${id}`).then((result) => {
-    console.log(result)
     return result.data.snacks
   })
 }
